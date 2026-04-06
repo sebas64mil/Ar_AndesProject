9eb64c62c2102d723ffcb01c6a87078c8fd786a6
Andryu18 <afclavijof@academia.usbbog.edu.co>
2026-04-06 13:33:39 -0500

    Node-modules-ignore

--- ARandess/src/modules/sceneManager.js
+++ ARandess/src/modules/sceneManager.js
@@ -12,12 +12,12 @@ export const sceneModule = () => {
       const {scene} = XR8.Threejs.xrScene()
 
       scene.add(root)
-/*
+
       const cube = createCube()
       cube.position.set(0, 0, 0)
 
       root.add(cube)
-*/
+
       const grid = new THREE.GridHelper(1000, 10)
       grid.position.set(0, -500, 0)
       root.add(grid)
